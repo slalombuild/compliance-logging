b120e3b7e08001c167bfc5a361ab91d3dc78b612
Carl-Philipp Harmant <cp.harmant@gmail.com>
2020-04-20 21:40:01 -0500

    Add lombok mask service

---
+++ compliance-common-logging/src/main/java/com/slalom/logging/compliance/common/LombokMaskService.java
@@ -0,0 +1,10 @@
+package com.slalom.logging.compliance.common;
+
+public class LombokMaskService implements MaskService {
+
+    @Override
+    public String maskMessage(String message) {
+        // TODO to implement
+        return null;
+    }
+}
