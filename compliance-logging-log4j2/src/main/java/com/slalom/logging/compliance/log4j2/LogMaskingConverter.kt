c97a903fb6c3fad84a3a56611c8309f49371c595
Carl-Philipp Harmant <cp.harmant@gmail.com>
2020-04-21 19:37:58 -0500

    Add dependency management for log4j2

--- compliance-logging-log4j2/src/main/java/com/slalom/logging/compliance/log4j2/LogMaskingConverter.kt
+++ compliance-logging-log4j2/src/main/java/com/slalom/logging/compliance/log4j2/LogMaskingConverter.kt
@@ -2,11 +2,9 @@ package com.slalom.logging.compliance.log4j2
 
 import org.apache.logging.log4j.core.LogEvent
 import org.apache.logging.log4j.core.pattern.LogEventPatternConverter
-import java.lang.StringBuilder
 
-class LogMaskingConverter(): LogEventPatternConverter("","") {
-    override fun format(event: LogEvent?, toAppendTo: StringBuilder?) {
+class LogMaskingConverter() : LogEventPatternConverter("", "") {
+    override fun format(event: LogEvent, toAppendTo: StringBuilder) {
         TODO("Not yet implemented")
     }
-
 }
